681b935eea375f86a7e29a3e8c51c771cf51c033
hoelsch <juergenhoelsch@gmail.com>
2017-03-30 12:04:08 +0200

    Replace some ES5 code with ES6

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-'use strict';
-
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
 const users = require('./routes/users');
+const groups = require('./routes/groups');
+const exercises = require('./routes/exercises');
+const exerciseTypes = require('./routes/exercise-types');
 
 const app = express();
 
@@ -13,25 +13,27 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', index);
 app.use('/users', users);
+app.use('/groups', groups);
+app.use('/exercises', exercises);
+app.use('/exercise-types', exerciseTypes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err);
 });
 
 module.exports = app;
