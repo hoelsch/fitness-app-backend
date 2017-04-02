63a3c7542849b9e17508362b5e9d3f6e3ed11eea
hoelsch <juergenhoelsch@gmail.com>
2017-04-02 18:21:06 +0200

    Add test case for GET exercise-types

---
+++ test/exercise-type.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const should = chai.should();
+const ExerciseType = require('../models').ExerciseType;
+
+chai.use(chaiHttp);
+
+describe('ExerciseType', function() {
+  beforeEach(function(done) {
+    ExerciseType.destroy({ where: {} }).then(function() {
+      done()
+    });
+  });
+
+  describe('GET /exercise-types', function() {
+    it('should list ALL exercise-types', function(done) {
+      const exerciseType = { name: 'Test' };
+      ExerciseType.create(exerciseType).then(function() {
+        chai.request(server)
+        .get('/exercise-types')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('exerciseType');
+          res.body.exerciseType.should.be.a('array');
+          res.body.exerciseType[0].should.have.property('id');
+          res.body.exerciseType[0].should.have.property('name');
+          res.body.exerciseType[0].should.have.property('createdAt');
+          res.body.exerciseType[0].should.have.property('updatedAt');
+          res.body.exerciseType[0].should.have.property('name').eql('Test');
+          done();
+        });
+      });
+    });
+  });
+});
