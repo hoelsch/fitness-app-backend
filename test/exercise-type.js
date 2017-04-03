c9f6820b5d40f76a6910b2a7b952b88f4460440e
hoelsch <juergenhoelsch@gmail.com>
2017-04-03 22:21:58 +0200

    Add test case for GET /exercise-types/:id

--- test/exercise-type.js
+++ test/exercise-type.js
@@ -17,21 +17,44 @@ describe('ExerciseType', function() {
     it('should list all exercise-types', function(done) {
       ExerciseType.create({ name: 'Test' }).then(function(newExerciseType) {
         chai.request(server)
-        .get('/exercise-types')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.should.be.a('object');
-          res.body.should.have.property('exerciseType');
-          res.body.exerciseType.should.be.a('array');
-          res.body.exerciseType[0].should.have.property('id');
-          res.body.exerciseType[0].should.have.property('name');
-          res.body.exerciseType[0].should.have.property('createdAt');
-          res.body.exerciseType[0].should.have.property('updatedAt');
-          res.body.exerciseType[0].should.have.property('id').eql(newExerciseType.id);
-          res.body.exerciseType[0].should.have.property('name').eql(newExerciseType.name);
-          done();
-        });
+          .get('/exercise-types')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('exerciseType');
+            res.body.exerciseType.should.be.a('array');
+            res.body.exerciseType[0].should.have.property('id');
+            res.body.exerciseType[0].should.have.property('name');
+            res.body.exerciseType[0].should.have.property('createdAt');
+            res.body.exerciseType[0].should.have.property('updatedAt');
+            res.body.exerciseType[0].should.have.property('id').eql(newExerciseType.id);
+            res.body.exerciseType[0].should.have.property('name').eql(newExerciseType.name);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('GET /exercise-types/:id', function() {
+    it('should get an exercise type with a given id', function(done) {
+      ExerciseType.create({ name: 'Test' }).then(function(newExerciseType) {
+        chai.request(server)
+          .get(`/exercise-types/${newExerciseType.id}`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('exerciseType');
+            res.body.exerciseType.should.be.a('object');
+            res.body.exerciseType.should.have.property('id');
+            res.body.exerciseType.should.have.property('name');
+            res.body.exerciseType.should.have.property('createdAt');
+            res.body.exerciseType.should.have.property('updatedAt');
+            res.body.exerciseType.should.have.property('id').eql(newExerciseType.id);
+            res.body.exerciseType.should.have.property('name').eql(newExerciseType.name);
+            done();
+          });
       });
     });
   });
