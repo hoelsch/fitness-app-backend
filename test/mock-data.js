b05a180f1aff0c83bb11225ca02708d112c0e7f4
hoelsch <juergenhoelsch@gmail.com>
2017-04-22 12:05:41 +0200

    Modify mock data used for testing

--- test/mock-data.js
+++ test/mock-data.js
@@ -1,7 +1,7 @@
 module.exports = {
   exercise: { note: 'Note' },
   exerciseType: { name: 'ExerciseType' },
-  set: { numReps: 0, weight: 0 },
+  set: { numReps: 10, weight: 100 },
   user: { name: 'User' },
   comment: { text: 'Comment' },
   group: { name: 'Group' },
