f59532ffb057b1fcd7a9cce1a5275eec69d0bbc5
hoelsch <juergenhoelsch@gmail.com>
2017-05-03 14:41:06 +0200

    Update timeout threshold for tests

--- test/helper.js
+++ test/helper.js
@@ -2,6 +2,7 @@ const models = require('../models');
 const server = require('../bin/www');
 
 before(function(done) {
+  this.timeout(15000);
   models.sequelize.sync().then(() => {
     server.listen(3000);
     server.on('listening', done);
