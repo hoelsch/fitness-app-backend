4ddbc46b4ad5491f925093ff2c1635bf5649acca
hoelsch <juergenhoelsch@gmail.com>
2017-06-05 21:09:38 +0200

    Add error handling in test helper

--- test/helper.js
+++ test/helper.js
@@ -1,10 +1,16 @@
+/* eslint no-undef: 0 */
+/* eslint func-names: 0 */
+
 const models = require('../models');
 const server = require('../bin/www');
 
-before(function(done) {
+before(function (done) {
   this.timeout(15000);
-  models.sequelize.sync().then(() => {
-    server.listen(3000);
-    server.on('listening', done);
-  });
+
+  models.sequelize.sync()
+    .then(() => {
+      server.listen(3000);
+      server.on('listening', done);
+    })
+    .catch(done);
 });
