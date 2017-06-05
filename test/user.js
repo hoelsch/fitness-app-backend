a697f3c849c7a84f2d70f65b23d5c9b5f89b7ed5
hoelsch <juergenhoelsch@gmail.com>
2017-06-05 21:07:59 +0200

    Add missing error handling to user route tests

--- test/user.js
+++ test/user.js
@@ -14,31 +14,36 @@ chai.use(chaiHttp);
 describe('User', () => {
   afterEach(function (done) {
     UserMock.deletePersistentInstances()
-      .then(() => done());
+      .then(() => done())
+      .catch(done);
   });
 
   describe('GET /users/:id', () => {
     it('should get a single user', function (done) {
-      UserMock.createPersistentInstance().then((result) => {
-        chai.request(server)
-          .get(`/users/${result.user.id}`)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('object');
+      UserMock.createPersistentInstance()
+        .then((result) => {
+          chai.request(server)
+            .get(`/users/${result.user.id}`)
+            .end((err, res) => {
+              if (err) done(err);
 
-            res.body.should.have.property('id');
-            res.body.id.should.equal(result.user.id);
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.should.be.a('object');
 
-            res.body.should.have.property('name');
-            res.body.name.should.equal(result.user.name);
+              res.body.should.have.property('id');
+              res.body.id.should.equal(result.user.id);
 
-            res.body.should.have.property('createdAt');
-            res.body.should.have.property('updatedAt');
+              res.body.should.have.property('name');
+              res.body.name.should.equal(result.user.name);
 
-            done();
-          });
-      });
+              res.body.should.have.property('createdAt');
+              res.body.should.have.property('updatedAt');
+
+              done();
+            });
+        })
+        .catch(done);
     });
     it('should return status code 404 for non-existing user', function (done) {
       chai.request(server)
@@ -58,6 +63,8 @@ describe('User', () => {
         .post('/users')
         .send(user)
         .end((err, res) => {
+          if (err) done(err);
+
           res.should.have.status(200);
           res.should.be.json;
           res.body.should.be.a('object');
@@ -87,27 +94,31 @@ describe('User', () => {
 
   describe('PATCH /users/:id', () => {
     it('should edit an user', function (done) {
-      UserMock.createPersistentInstance().then((result) => {
-        chai.request(server)
-          .patch(`/users/${result.user.id}`)
-          .send({ name: 'Updated' })
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('object');
+      UserMock.createPersistentInstance()
+        .then((result) => {
+          chai.request(server)
+            .patch(`/users/${result.user.id}`)
+            .send({ name: 'Updated' })
+            .end((err, res) => {
+              if (err) done(err);
 
-            res.body.should.have.property('id');
-            res.body.id.should.equal(result.user.id);
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.should.be.a('object');
 
-            res.body.should.have.property('name');
-            res.body.name.should.equal('Updated');
+              res.body.should.have.property('id');
+              res.body.id.should.equal(result.user.id);
 
-            res.body.should.have.property('createdAt');
-            res.body.should.have.property('updatedAt');
+              res.body.should.have.property('name');
+              res.body.name.should.equal('Updated');
 
-            done();
-          });
-      });
+              res.body.should.have.property('createdAt');
+              res.body.should.have.property('updatedAt');
+
+              done();
+            });
+        })
+        .catch(done);
     });
     it('should return status code 404 for non-existing user', function (done) {
       chai.request(server)
@@ -120,30 +131,36 @@ describe('User', () => {
         });
     });
     it('should return status code 400 for invalid input', function (done) {
-      UserMock.createPersistentInstance().then((result) => {
-        chai.request(server)
-          .patch(`/users/${result.user.id}`)
-          .send({})
-          .end((err, res) => {
-            res.should.have.status(400);
+      UserMock.createPersistentInstance()
+        .then((result) => {
+          chai.request(server)
+            .patch(`/users/${result.user.id}`)
+            .send({})
+            .end((err, res) => {
+              res.should.have.status(400);
 
-            done();
-          });
-      });
+              done();
+            });
+        })
+        .catch(done);
     });
   });
 
   describe('DELETE /users/:id', () => {
     it('should delete an user', function (done) {
-      UserMock.createPersistentInstance().then((result) => {
-        chai.request(server)
-          .delete(`/users/${result.user.id}`)
-          .end((err, res) => {
-            res.should.have.status(204);
+      UserMock.createPersistentInstance()
+        .then((result) => {
+          chai.request(server)
+            .delete(`/users/${result.user.id}`)
+            .end((err, res) => {
+              if (err) done(err);
 
-            done();
-          });
-      });
+              res.should.have.status(204);
+
+              done();
+            });
+        })
+        .catch(done);
     });
     it('should return status code 404 for non-existing user', function (done) {
       chai.request(server)
@@ -158,40 +175,48 @@ describe('User', () => {
 
   describe('GET /users/:id/groups', () => {
     it('should list groups of an user', function (done) {
-      UserMock.createPersistentInstance().then((result) => {
-        chai.request(server)
-          .get(`/users/${result.user.id}/groups`)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
+      UserMock.createPersistentInstance()
+        .then((result) => {
+          chai.request(server)
+            .get(`/users/${result.user.id}/groups`)
+            .end((err, res) => {
+              if (err) done(err);
 
-            res.body[0].should.be.a('object');
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.should.be.a('array');
 
-            res.body[0].should.have.property('id');
-            res.body[0].id.should.equal(result.group.id);
+              res.body[0].should.be.a('object');
 
-            res.body[0].should.have.property('name');
-            res.body[0].should.have.property('createdAt');
-            res.body[0].should.have.property('updatedAt');
+              res.body[0].should.have.property('id');
+              res.body[0].id.should.equal(result.group.id);
 
-            done();
-          });
-      });
+              res.body[0].should.have.property('name');
+              res.body[0].should.have.property('createdAt');
+              res.body[0].should.have.property('updatedAt');
+
+              done();
+            });
+        })
+        .catch(done);
     });
     it('should return empty array when an user has no groups', function (done) {
-      User.create(UserMock.getMockData()).then((user) => {
-        chai.request(server)
-          .get(`/users/${user.id}/groups`)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
-            res.body.should.be.empty;
+      User.create(UserMock.getMockData())
+        .then((user) => {
+          chai.request(server)
+            .get(`/users/${user.id}/groups`)
+            .end((err, res) => {
+              if (err) done(err);
 
-            done();
-          });
-      });
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.should.be.a('array');
+              res.body.should.be.empty;
+
+              done();
+            });
+        })
+        .catch(done);
     });
     it('should return status code 404 for non-existing user', function (done) {
       chai.request(server)
@@ -206,57 +231,65 @@ describe('User', () => {
 
   describe('GET /users/:id/exercises', () => {
     it('should list exercises of an user', function (done) {
-      UserMock.createPersistentInstance().then((result) => {
-        chai.request(server)
-          .get(`/users/${result.user.id}/exercises`)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
+      UserMock.createPersistentInstance()
+        .then((result) => {
+          chai.request(server)
+            .get(`/users/${result.user.id}/exercises`)
+            .end((err, res) => {
+              if (err) done(err);
 
-            res.body[0].should.be.a('object');
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.should.be.a('array');
 
-            res.body[0].should.have.property('id');
-            res.body[0].id.should.equal(result.exercise.id);
+              res.body[0].should.be.a('object');
 
-            res.body[0].should.have.property('note');
-            res.body[0].note.should.equal(result.exercise.note);
+              res.body[0].should.have.property('id');
+              res.body[0].id.should.equal(result.exercise.id);
 
-            res.body[0].should.have.property('sets');
-            res.body[0].sets.should.be.a('array');
+              res.body[0].should.have.property('note');
+              res.body[0].note.should.equal(result.exercise.note);
 
-            res.body[0].should.have.property('createdAt');
-            res.body[0].should.have.property('updatedAt');
+              res.body[0].should.have.property('sets');
+              res.body[0].sets.should.be.a('array');
 
-            res.body[0].should.have.property('user');
-            res.body[0].user.should.be.a('object');
-            res.body[0].user.should.have.property('id');
-            res.body[0].user.id.should.equal(result.user.id);
-            res.body[0].user.should.have.property('name');
-            res.body[0].user.name.should.equal(result.user.name);
+              res.body[0].should.have.property('createdAt');
+              res.body[0].should.have.property('updatedAt');
 
-            res.body[0].should.have.property('exerciseType');
-            res.body[0].exerciseType.should.be.a('object');
-            res.body[0].exerciseType.should.have.property('name');
-            res.body[0].exerciseType.name.should.equal(result.exerciseType.name);
+              res.body[0].should.have.property('user');
+              res.body[0].user.should.be.a('object');
+              res.body[0].user.should.have.property('id');
+              res.body[0].user.id.should.equal(result.user.id);
+              res.body[0].user.should.have.property('name');
+              res.body[0].user.name.should.equal(result.user.name);
 
-            done();
-          });
-      });
+              res.body[0].should.have.property('exerciseType');
+              res.body[0].exerciseType.should.be.a('object');
+              res.body[0].exerciseType.should.have.property('name');
+              res.body[0].exerciseType.name.should.equal(result.exerciseType.name);
+
+              done();
+            });
+        })
+        .catch(done);
     });
     it('should return empty array when an user has no exercises', function (done) {
-      User.create(UserMock.getMockData()).then((user) => {
-        chai.request(server)
-          .get(`/users/${user.id}/exercises`)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('array');
-            res.body.should.be.empty;
+      User.create(UserMock.getMockData())
+        .then((user) => {
+          chai.request(server)
+            .get(`/users/${user.id}/exercises`)
+            .end((err, res) => {
+              if (err) done(err);
 
-            done();
-          });
-      });
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.should.be.a('array');
+              res.body.should.be.empty;
+
+              done();
+            });
+        })
+        .catch(done);
     });
     it('should return status code 404 for non-existing user', function (done) {
       chai.request(server)
@@ -271,20 +304,24 @@ describe('User', () => {
 
   describe('GET /users/:id/statistics', () => {
     it('should get statistics of an user', function (done) {
-      UserMock.createPersistentInstance().then((result) => {
-        chai.request(server)
-          .get(`/users/${result.user.id}/statistics`)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.should.be.json;
-            res.body.should.be.a('object');
+      UserMock.createPersistentInstance()
+        .then((result) => {
+          chai.request(server)
+            .get(`/users/${result.user.id}/statistics`)
+            .end((err, res) => {
+              if (err) done(err);
 
-            res.body.should.have.property('totalWeightLifted');
-            res.body.totalWeightLifted.should.equal(0);
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.should.be.a('object');
 
-            done();
-          });
-      });
+              res.body.should.have.property('totalWeightLifted');
+              res.body.totalWeightLifted.should.equal(0);
+
+              done();
+            });
+        })
+        .catch(done);
     });
   });
 });
